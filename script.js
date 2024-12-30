0180a19eaadd1640b6b194ffa96e43c26d57d30a
ivanse22 <ivan.se22@gmail.com>
2024-12-29 21:45:11 -0500

    Set initial files to the github repository

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hola Mundo");
+
+let nombre = "Ivan";
+let platoPrincipal = "Hamburguesa";
+let acompañaminento = "Papas Fritas";
+const restaurante = "Gordo";
+let cantidadHamburguesas = 5;
